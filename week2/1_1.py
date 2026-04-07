eafe15156a9636f16cc707e5c0639ab651b78016
Dungxnguyenzzz <titmin165@gmail.com>
2026-04-07 08:13:24 +0700

    Create 1_1.py

---
+++ week2/1_1.py
@@ -0,0 +1,5 @@
+print("1.1. Có bao nhiêu giây trong m phút n giây")
+m = float(input("Nhập số phút: "))
+n = int(input("Nhập số giây: "))
+
+print(f"Số giây trong {m} phút và {n} giây là: {m * 60 + n}")
